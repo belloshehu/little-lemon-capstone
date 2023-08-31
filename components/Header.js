dc92963f17d69d84dd3ea9d4b52da59f9bb289ce
Belloshehu <belloshehu1@gmail.com>
2023-09-01 00:05:30 +0100

    Finish home screen without filter functionality

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,37 @@
-import { StyleSheet, Image, View } from "react-native";
+import { StyleSheet, Image, View, Pressable } from "react-native";
 
-export const Header = () => (
-  <View style={styles.wrapper}>
-    <Image source={require("../assets/Logo.png")} style={styles.logo} />
-    <Image source={require("../assets/Profile.png")} style={styles.profile} />
-  </View>
-);
+export const Header = ({ navigation }) => {
+  return (
+    <View style={styles.wrapper}>
+      <Image source={require("../assets/Logo.png")} style={styles.logo} />
+      <Pressable
+        onPress={() => {
+          navigation.navigate("Profile");
+        }}>
+        <Image
+          source={require("../assets/Profile.png")}
+          style={styles.profile}
+        />
+      </Pressable>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   wrapper: {
     flexDirection: "row",
     gap: 20,
     alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+    width: "100%",
   },
   logo: {
     resizeMode: "cover",
-    width: 100,
-    height: 30,
+    height: 40,
   },
   profile: {
-    resizeMode: "contain",
+    resizeMode: "cover",
     width: 40,
     height: 40,
     borderRadius: 100,
--- screens/Home.js
+++ screens/Home.js
@@ -1,30 +1,173 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
-import { deleteProfile, updateProfile } from "../utills/storage";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  TextInput,
+  Image,
+  ActivityIndicator,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { CategorySection } from "../components/CategorySection";
+import { MealItemList } from "../components/MealItemList";
+import { useLayoutEffect, useState } from "react";
+import { createTable, getMenuItems, saveMenuItems } from "../database";
+
+const mealsData = [
+  {
+    name: "Buretta",
+    price: 20,
+    category: "Starters",
+    id: 1,
+  },
+  {
+    name: "Pasta",
+    price: 20.99,
+    category: "Mains",
+    id: 1,
+  },
+  {
+    name: "Greek salad",
+    price: 20,
+    category: "Deserts",
+    id: 1,
+  },
+];
+const categories = mealsData.map((meal) => meal.category);
 
 export const Home = ({ navigation }) => {
+  const [menuItems, setMenuItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json"
+      );
+      const jsonData = await response.json();
+      return jsonData.menu;
+    } catch (error) {
+      console.log("Error occured: " + error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useLayoutEffect(() => {
+    (async () => {
+      try {
+        await createTable();
+        setLoading(true);
+        let items = await getMenuItems();
+        if (items.length === 0) {
+          items = await fetchData();
+          saveMenuItems(items);
+        }
+        setMenuItems(items);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.info}>Home</Text>
-      <Pressable
-        style={styles.button}
-        onPress={() => navigation.navigate("Profile")}>
-        <Text style={styles.buttonText}>View profile</Text>
-      </Pressable>
-      <Pressable style={styles.button} onPress={() => deleteProfile()}>
-        <Text style={styles.buttonText}>Log out</Text>
-      </Pressable>
+      <View style={styles.heroSection}>
+        <Text style={styles.heroHeadingText}>Little lemon</Text>
+        <View style={styles.heroImageSection}>
+          <View style={styles.heroDescriptionWrapper}>
+            <Text style={styles.heroSubText}>Chicago</Text>
+            <Text style={styles.heroDescriptionText}>
+              We are a family owned Mediterranean restaurant, focused on
+              tradional recipes served with modern twist.
+            </Text>
+          </View>
+          <Image
+            source={require("../assets/hero-image.png")}
+            style={styles.heroImage}
+          />
+        </View>
+        <View style={styles.inputWrapper}>
+          <AntDesign name="search1" size={24} color="black" />
+          <TextInput placeholder="Search for a meal" style={styles.input} />
+        </View>
+      </View>
+
+      {loading ? (
+        <View style={styles.indicatorWrapper}>
+          <ActivityIndicator />
+        </View>
+      ) : (
+        <MealItemList menuData={menuItems} />
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  indicatorWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 10,
+    paddingRight: 20,
+  },
+  inputWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    width: "100%",
+    backgroundColor: "#fff",
+    marginTop: 10,
+    borderRadius: 10,
+  },
   container: {
     width: "100%",
     flex: 1,
     backgroundColor: "#fefefe",
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
-    gap: 20,
+  },
+  heroSection: {
+    backgroundColor: "#475e59",
+    width: "100%",
+    gap: 5,
+    padding: 20,
+  },
+  heroHeadingText: {
+    fontSize: 40,
+    color: "#F4CE14",
+    fontWeight: "bold",
+  },
+  heroImageSection: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+  },
+  heroImage: {
+    borderRadius: 20,
+    resizeMode: "cover",
+    width: 120,
+    height: 150,
+  },
+  heroDescriptionWrapper: {
+    gap: 10,
+    textAlign: "left",
+    width: "55%",
+  },
+  heroDescriptionText: {
+    fontSize: 16,
+    color: "#fff",
+  },
+  heroSubText: {
+    color: "#fefefe",
+    fontSize: 24,
+    fontWeight: "bold",
   },
   info: {
     fontSize: 24,
